4e01d450ed7be85c7af7d2c53eb215015241ac86
Exalted02 <exaltedsol02@gmail.com>
2025-08-07 20:02:54 +0530

    up

---
+++ resources/views/_includes/header.blade.php
@@ -0,0 +1,8 @@
+<!-- Header -->
+<div class="custom-header text-center">
+	<div class="decor-top-left"></div>
+	<a href="{{ route('dashboard')}}" class="logo">
+		<img src="{{ asset('front-assets/img/-logo1.png') }}" alt="{{ __('project_title') }}">
+	</a>
+</div>
+<!-- /Header -->
---
+++ resources/views/admin/email-management/index.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.app')
+
+@section('styles')
+<link rel="stylesheet" href="{{ url('front-assets/plugins/summernote/summernote-bs4.min.css') }}">
+@endsection 
+@section('content')
+<!-- Page Wrapper -->
+<div class="page-wrapper">
+	<!-- Page Content -->
+	<div class="content container-fluid">
+	
+		<!-- Page Header -->
+		<div class="page-header">
+			<div class="row align-items-center">
+				<div class="col-md-4">
+					<h3 class="page-title">{{ __('email_management') }}</h3>
+					<ul class="breadcrumb">
+						<li class="breadcrumb-item"><a href="{{ route('dashboard') }}">{{ __('dashboard') }}</a></li>
+						<li class="breadcrumb-item active">{{ __('email_management') }}</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- /Page Header -->
+		<hr>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="table-responsive">
+					<table class="table table-striped custom-table datatable">
+						<thead>
+							<tr>
+								<th>{{ __('name') }}</th>
+								<th>{{ __('email_subject') }}</th>
+								<th>{{ __('status') }}</th>
+								<th class="text-end">{{ __('action') }}</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($data as $val)
+							<tr>
+								<td>{{ $val->name ?? ''}}</td>
+								<td>{{ $val->message_subject ?? ''}}</td>
+								<td>
+								@if($val->status ==1)
+									<div class="dropdown action-label">
+										<a class="btn btn-white btn-sm badge-outline-success dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+											<i class="fa-regular fa-circle-dot text-success"></i> {{ __('active') }}
+										</a>
+										<div class="dropdown-menu dropdown-menu-right">
+											<a class="dropdown-item update-status" href="javascript:void(0);" data-id="{{ $val->id }}" data-url="{{ route('email-management-update-status') }}"><i class="fa-regular fa-circle-dot text-success"></i> {{ __('active') }}</a>
+											<a class="dropdown-item update-status" href="javascript:void(0);" data-id="{{ $val->id }}" data-url="{{ route('email-management-update-status') }}"><i class="fa-regular fa-circle-dot text-danger"></i> {{ __('inactive') }}</a>
+										</div>
+									</div>
+								 @else
+									<div class="dropdown action-label">
+										<a class="btn btn-white btn-sm badge-outline-danger dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+											<i class="fa-regular fa-circle-dot text-danger"></i> {{ __('inactive') }}
+										</a>
+										<div class="dropdown-menu dropdown-menu-right">
+											<a class="dropdown-item update-status" href="javascript:void(0);" data-id="{{ $val->id }}" data-url="{{ route('email-management-update-status') }}"><i class="fa-regular fa-circle-dot text-success"></i> {{ __('active') }}</a>
+											<a class="dropdown-item update-status" href="javascript:void(0);" data-id="{{ $val->id }}" data-url="{{ route('email-management-update-status') }}"><i class="fa-regular fa-circle-dot text-danger"></i> {{ __('inactive') }}</a>
+										</div>
+									</div> 
+								 
+								 @endif
+								</td>
+								<td class="text-end">
+									<div class="dropdown dropdown-action">
+										<a href="#" class="action-icon dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
+										<div class="dropdown-menu dropdown-menu-right">
+											<a class="dropdown-item edit-product-code" href="{{ route('email-management-edit', $val->id) }}"><i class="fa-solid fa-pencil m-r-5"></i> {{ __('edit') }}</a>
+										</div>
+									</div>
+								</td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	<!-- /Page Content -->
+@include('modal.email-management-modal')
+@include('modal.common')
+@endsection 
+@section('scripts')
+<script src="{{ url('front-assets/js/page/email_management.js') }}"></script>
+<script src="{{ url('front-assets/plugins/summernote/summernote-bs4.min.js') }}"></script>
+<script>
+//var csrfToken = "{{ csrf_token() }}";
+$( document ).ready(function() {
+	if ($.fn.DataTable.isDataTable('.datatable')) {
+		$('.datatable').DataTable().destroy(); // Destroy existing instance
+	}
+
+	$('.datatable').DataTable({
+		//searching: false,
+		language: {
+			"lengthMenu": "{{ __('Show _MENU_ entries') }}",
+			"zeroRecords": "{{ __('No records found') }}",
+			"info": "{{ __('Showing _START_ to _END_ of _TOTAL_ entries') }}",
+			"infoEmpty": "{{ __('No entries available') }}",
+			"infoFiltered": "{{ __('(filtered from _MAX_ total entries)') }}",
+			"search": "{{ __('search') }}",
+			"paginate": {
+				"first": "{{ __('First') }}",
+				"last": "{{ __('Last') }}",
+				"next": "{{ __('Next') }}",
+				"previous": "{{ __('Previous') }}"
+			},
+		}
+	});
+});
+</script>
+@endsection
