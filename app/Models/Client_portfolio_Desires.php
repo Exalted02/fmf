96d3d6c38ef2f93b675d47ad24c146798495e225
exaltedsol04 <exaltedsol04@gmail.com>
2025-08-12 12:17:41 +0530

    comment

---
+++ app/Models/Client_portfolio_Desires.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client_portfolio_Desires extends Model
+{
+    use HasFactory;
+	protected $fillable = [
+        'user_id',
+		'client_name',
+		'client_age',
+		'partner_name',
+		'partner_age',
+		'current_portfolio_value',
+		'desired_gross_income_retirement',
+		'desired_retirement_age',
+		'COLA',
+		'cola_age',
+		'assumed_return',
+		'RIPG',
+		'status', 
+    ];
+}
---
+++ database/migrations/2025_08_12_061101_create_client_portfolio__desires_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client_portfolio_desires', function (Blueprint $table) {
+            $table->id();
+			$table->integer('user_id')->nullable();
+			$table->string('client_name')->nullable();
+			$table->integer('client_age')->nullable();
+			$table->string('partner_name')->nullable();
+			$table->integer('partner_age')->nullable();
+			$table->double('current_portfolio_value', 8, 2)->nullable();
+			$table->double('desired_gross_income_retirement', 8, 2)->nullable();
+			$table->integer('desired_retirement_age')->nullable();
+			$table->integer('COLA')->nullable()->comment('Cost of Living Adjustment');
+			$table->integer('cola_age')->nullable()->comment('Age to Begin COLA Adjustment');
+			$table->integer('assumed_return')->nullable();
+			$table->string('RIPG')->nullable()->comment('1=Income,2=Tax Reduction,3=Legacy');
+			$table->tinyInteger('status')->default(1)->comment('1=active ,0=inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client_portfolio__desires');
+    }
+};
