3366422b8e8f4294b188abfb9e920a60e02f03c1
exaltedsol04 <exaltedsol04@gmail.com>
2025-09-04 11:41:08 +0530

    COMMENT

--- resources/views/income-plan-pdf.blade.php
+++ resources/views/income-plan-pdf.blade.php
@@ -547,6 +547,7 @@ $husband_account_value = $current_finance_husband_data ? $current_finance_husban
 			$a14 = 0;
 			$a17 = 0;
 			$a20 = 0;
+			$J_16 = 0;
 			$index17_previous = 0;
 			$index19_previous = 0;
 		@endphp
@@ -658,11 +659,68 @@ $husband_account_value = $current_finance_husband_data ? $current_finance_husban
 											{
 												$C16 = round($a20-$index15_previous);
 											}
+											
+											if($col == 4)
+											{
+												$D_16= $a20-$index15_previous;
+											}
+											
+											if($col == 5)
+											{
+												$E_16= $a20-$index15_previous;
+											}
+											
+											if($col == 6)
+											{
+												$F_16= $a20-$index15_previous;
+											}
+											
+											if($col == 7)
+											{
+												$G_16= $a20-$index15_previous;
+											}
+											
+											if($col == 8)
+											{
+												$H_16= $a20-$index15_previous;
+											}
+											
 											@endphp
 										@elseif($row == 6 && $col<=8)
 										{{ number_format(round(($index12_previous-$index13_previous-$a20) * 1.05)) }}
 											@php
 											    $index17_previous = ($index12_previous-$index13_previous-$a20) * 1.05;
+												
+												if($col == 3)
+												{
+													$C_17 = ($index12_previous-$index13_previous-$a20) * 1.05;
+												}
+												
+												if($col == 4)
+												{
+													$D_17 = ($index12_previous-$index13_previous-$a20) * 1.05;
+												}
+												
+												if($col == 5)
+												{
+													$E_17 = ($index12_previous-$index13_previous-$a20) * 1.05;
+												}
+												
+												if($col == 6)
+												{
+													$F_17 = ($index12_previous-$index13_previous-$a20) * 1.05;
+												}
+												
+												if($col == 7)
+												{
+													$G_17 = ($index12_previous-$index13_previous-$a20) * 1.05;
+												}
+												
+												if($col == 8)
+												{
+													$H_17 = ($index12_previous-$index13_previous-$a20) * 1.05;
+												}
+												
 											@endphp
 										@elseif($row == 7 && $col<=8)
 											@php
@@ -747,10 +805,114 @@ $husband_account_value = $current_finance_husband_data ? $current_finance_husban
 										@endif
 									@endif
 									
-									@if($col>9)
+									@if($col>10)
 										@if($col==11 && $row<=7)
 											year{{$row}}
 										@endif
+										
+										@if($col==12)
+											@if($row == 1)
+												${{ number_format(round($C16*1.05))  }}
+												@php 
+												  $L_14 = $C16*1.05;
+												@endphp
+											@endif
+											
+											@if($row == 2)
+												${{ number_format(round($L_14+ $D_16) * 1.05)  }}
+												@php 
+												  $L_15 = ($L_14+ $D_16) * 1.05;
+												@endphp
+											@endif
+											
+											@if($row == 3)
+												${{ number_format(round($L_15+ $E_16) * 1.05)  }}
+												@php 
+												  $L_16 = ($L_15+ $E_16) * 1.05;
+												@endphp
+											@endif
+											
+											@if($row == 4)
+												${{ number_format(round($L_16+ $F_16) * 1.05)  }}
+												@php 
+												  $L_17 = ($L_16+ $F_16) * 1.05;
+												@endphp
+											@endif
+											
+											@if($row == 5)
+												${{ number_format(round($L_17+ $G_16) * 1.05)  }}
+												@php 
+												  $L_18 = ($L_17+ $G_16) * 1.05;
+												@endphp
+											@endif
+											
+											@if($row == 6)
+												${{ number_format(round($L_18+ $H_16) * 1.05)  }}
+												@php 
+												  $L_19 = ($L_18+ $H_16) * 1.05;
+												@endphp
+											@endif
+											
+											@if($row == 7)
+											  ${{ number_format(round($L_19+ $i16) * 1.05)  }}
+												@php 
+												  $L_20 = ($L_19+ $i16) * 1.05;
+												@endphp
+											@endif
+											
+										@endif
+										
+										@if($col==13)
+											@if($row == 1)
+												${{ number_format(round($C_17+$L_14))  }}
+												@php 
+												  $M_14 = $C_17+$L_14;
+												@endphp
+											@endif
+											
+											@if($row == 2)
+												${{ number_format(round($D_17+$L_15))  }}
+												@php 
+												  $M_15 = $D_17+$L_15;
+												@endphp
+											@endif
+											
+											@if($row == 3)
+												${{ number_format(round($E_17+$L_16))  }}
+												@php 
+												  $M_16 = $E_17+$L_16;
+												@endphp
+											@endif
+											
+											@if($row == 4)
+												${{ number_format(round($F_17+$L_17))  }}
+												@php 
+												  $M_17 = $F_17+$L_17;
+												@endphp
+											@endif
+											
+											@if($row == 5)
+												${{ number_format(round($G_17+$L_18))  }}
+												@php 
+												  $M_18 = $G_17+$L_18;
+												@endphp
+											@endif
+											
+											@if($row == 6)
+												${{ number_format(round($H_17+$L_19))  }}
+												@php 
+												  $M_19 = $H_17+$L_19;
+												@endphp
+											@endif
+											
+											@if($row == 7)
+												${{ number_format(round(($J_16+$L_20) * 1.05))  }}
+												@php 
+												  $M_20 = ($J_16+$L_20) * 1.05;
+												@endphp
+											@endif
+										@endif
+									
 									@endif
 									</td>
 								</tr>
